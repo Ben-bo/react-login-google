a5b0a8957bfbe893d944aa6027996e3ca4b2f60d
unknown <bennyizkandar@gmail.com>
2021-12-16 10:11:42 +0800

    add function register data user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { Container } from "react-bootstrap";
+import GoogleLogin from "react-google-login";
+import "./App.css";
+import NavbarComponent from "./component/NavbarComponent";
 
 function App() {
+  const responseSuccessGoogle = (res) => {
+    console.log(res);
+    axios({
+      method: "POST",
+      url: "http://localhost:5000/api/login",
+      data: { tokenId: res.tokenId },
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+  const responseErrorGoogle = (res) => {
+    console.log(res);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+      <Container className="mt-3">
+        <h3 className="text-center my-3"> login With Google</h3>
+        <GoogleLogin
+          clientId="829363947247-2n7d9ui3t25r7ep3cba83jd0kg9rph65.apps.googleusercontent.com"
+          buttonText="Login with google"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+      </Container>
     </div>
   );
 }
